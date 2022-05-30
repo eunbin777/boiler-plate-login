7751c23a7351708b53815971e04ebc82b2a6c951
eunbin777 <103042314+eunbin777@users.noreply.github.com>
2022-05-30 15:25:12 +0900

    change structure & logout error

--- server/index.js
+++ server/index.js
@@ -20,15 +20,19 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected..'))
   .catch(err => console.log(err))
 
+app.get('/api/hello', (req, res) =>{
+    res.send("Hello World!")
+})
+
 app.get('/', (req, res) => res.send('Hello World!!!'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     //회원 가입할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다.
 
     const user = new User(req.body)
-    user.save((err, doc) => {
+    user.save((err, userInfo) => {
         if(err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
@@ -60,8 +64,8 @@ app.post('/api/users/login', (req, res) => {
 
                 //토큰을 저장한다. 어디에? 쿠키? 로컬스토리지?
                 res.cookie("x_auth", user.token)
-                .status(200)
-                .json({ loginSuccess: true, userId: user._id })
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
 
             })
         })
@@ -74,7 +78,8 @@ app.get('/api/users/auth', auth, (req, res)=>{
     //미들웨어를 통과해왔다는 얘기는 Authentication이 True라는 말
     res.status(200).json({
         _id: req.user._id,
-        isAdin: req.user.role === 0 ? false : true,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
         email: req.user.email,
         name: req.user.name,
         lastname: req.user.lastname,
@@ -82,17 +87,16 @@ app.get('/api/users/auth', auth, (req, res)=>{
         image: req.user.image
     })
 })
-
 app.get('/api/users/logout', auth, (req, res) => {
-
-    User.findOneAndUpdate({_id: req.user._id},
-        {token: ""},
-        (err, user) =>{
-            if(err) return res.json({success: false, err});
-            return res.status(200).send({
-                success: true
-            })
+    console.log("logout")
+    User.findOneAndUpdate({ _id: req.user._id },
+      { token: "" }
+      , (err, user) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send({
+          success: true
         })
-})
+      })
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
